1b6c620a390038672b5cb943cfe34243fc49d40a
Heitor Colangelo <heitor.colangelo@monese.com>
2020-05-04 18:08:57 +0100

    Add json adapter for dates, make mappers in data layer internal

--- Data-Remote/src/main/kotlin/com/heitorcolangelo/data/remote/di/RemoteDataModule.kt
+++ Data-Remote/src/main/kotlin/com/heitorcolangelo/data/remote/di/RemoteDataModule.kt
@@ -3,7 +3,9 @@ package com.heitorcolangelo.data.remote.di
 import com.heitorcolangelo.data.remote.common.BuildConfiguration
 import com.heitorcolangelo.data.remote.common.api.AuthInterceptor
 import com.heitorcolangelo.data.remote.common.api.OkHttpClientFactory
+import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
 import dagger.Module
 import dagger.Provides
 import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
@@ -14,6 +16,7 @@ import retrofit2.CallAdapter
 import retrofit2.Converter
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.io.File
+import java.util.Date
 
 @Module
 class RemoteDataModule(private val configuration: BuildConfiguration) {
@@ -27,9 +30,16 @@ class RemoteDataModule(private val configuration: BuildConfiguration) {
     }
 
     @Provides
-    fun provideConverterFactory(): Converter.Factory {
-        val moshi = Moshi.Builder().build()
+    fun provideConverterFactory(dateAdapter: JsonAdapter<Date>): Converter.Factory {
+        val moshi = Moshi.Builder()
+            .add(Date::class.java, dateAdapter)
+            .build()
         return MoshiConverterFactory.create(moshi)
+    }
+
+    @Provides
+    fun provideDateAdapter(): JsonAdapter<Date> {
+        return Rfc3339DateJsonAdapter().nullSafe()
     }
 
     @Provides
--- Data/src/main/kotlin/com/heitorcolangelo/data/common/model/DataDomainMapper.kt
+++ Data/src/main/kotlin/com/heitorcolangelo/data/common/model/DataDomainMapper.kt
@@ -2,12 +2,12 @@ package com.heitorcolangelo.data.common.model
 
 import com.heitorcolangelo.domain.common.model.DomainModel
 
-interface DataDomainMapper<Data : DataModel, Domain : DomainModel> {
+internal interface DataDomainMapper<Data : DataModel, Domain : DomainModel> {
     fun mapToDomainModel(dataModel: Data): Domain
     fun mapToDataModel(domainModel: Domain): Data
 }
 
-interface DataListDomainMapper<Data : DataModel, Domain : DomainModel> {
+internal interface DataListDomainMapper<Data : DataModel, Domain : DomainModel> {
     fun mapToDomainModel(dataModelList: List<Data>): Domain
     fun mapToDataModelList(domainModel: Domain): List<Data>
 }
