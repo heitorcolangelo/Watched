4d858df116daa2c441c05978281976e33a3b42ef
Heitor Colangelo <heitor.colangelo@monese.com>
2020-05-06 06:19:50 +0100

    Remove dummy feature (UI and Remote data)

--- dummy/src/main/kotlin/com/heitorcolangelo/dummy/model/DummyUiModel.kt
+++
@@ -1,7 +0,0 @@
-package com.heitorcolangelo.dummy.model
-
-import com.heitorcolangelo.presentation.common.model.UiModel
-
-data class DummyUiModel(
-    val dummyId: String
-) : UiModel(dummyId)
--- dummy/src/test/kotlin/com/heitorcolangelo/domain/factory/DummyDomainModelFactory.kt
+++
@@ -1,9 +0,0 @@
-package com.heitorcolangelo.domain.factory
-
-import com.heitorcolangelo.domain.dummy.model.DummyDomainModel
-
-internal object DummyDomainModelFactory : MockFactory<DummyDomainModel> {
-    override fun make(): DummyDomainModel {
-        return DummyDomainModel(randomId())
-    }
-}
