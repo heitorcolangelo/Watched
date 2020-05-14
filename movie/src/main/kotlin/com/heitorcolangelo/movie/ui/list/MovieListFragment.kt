8c1dccda88baea70cfa10f222b91fbc93220007c
Heitor Colangelo <heitor.colangelo@monese.com>
2020-05-14 16:45:10 +0100

    UI tweaks

--- movie/src/main/kotlin/com/heitorcolangelo/movie/ui/list/MovieListFragment.kt
+++ movie/src/main/kotlin/com/heitorcolangelo/movie/ui/list/MovieListFragment.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import com.google.android.flexbox.AlignContent
+import com.google.android.flexbox.AlignItems
 import com.google.android.flexbox.FlexDirection
+import com.google.android.flexbox.FlexWrap
 import com.google.android.flexbox.FlexboxLayoutManager
 import com.google.android.flexbox.JustifyContent
 import com.heitorcolangelo.movie.R
@@ -68,7 +71,7 @@ class MovieListFragment : Fragment(R.layout.fragment_movie_list) {
     private fun setupLayoutManager(): FlexboxLayoutManager {
         return FlexboxLayoutManager(requireContext()).apply {
             flexDirection = FlexDirection.ROW
-            justifyContent = JustifyContent.SPACE_BETWEEN
+            justifyContent = JustifyContent.SPACE_EVENLY
         }
     }
 }
