bf1f68d7b9c405ff48c89b65cff4bc14c934bec7
Heitor Colangelo <heitor.colangelo@monese.com>
2020-05-27 22:56:33 +0100

    Add pagination to domain layer

---
+++ Domain/src/main/kotlin/com/heitorcolangelo/domain/common/scheduler/PagedObserver.kt
@@ -0,0 +1,30 @@
+package com.heitorcolangelo.domain.common.scheduler
+
+import com.heitorcolangelo.domain.common.model.DomainModel
+import com.heitorcolangelo.domain.common.model.PageDomainModel
+import io.reactivex.rxjava3.observers.DisposableObserver
+
+abstract class PagedObserver<Model: DomainModel> :
+    DisposableObserver<PageDomainModel<Model>>() {
+
+    override fun onComplete() {
+    }
+
+    override fun onNext(newPage: PageDomainModel<Model>) {
+        onLoadPageSuccess(newPage)
+    }
+
+    override fun onError(error: Throwable) {
+        onLoadPageFailure(error)
+    }
+
+    abstract fun onLoadPageSuccess(newPage: PageDomainModel<Model>)
+    abstract fun onLoadPageFailure(error: Throwable)
+
+    /**
+     * Implement this method to handle the case when you request a new page but the current cache
+     * is not valid anymore.
+     * Ideally you should clean the current list in order to get up-to-date data.
+     */
+    abstract fun invalidatePages()
+}
---
+++ Domain/src/main/kotlin/com/heitorcolangelo/domain/common/usecase/PagedUseCase.kt
@@ -0,0 +1,47 @@
+package com.heitorcolangelo.domain.common.usecase
+
+import com.heitorcolangelo.domain.common.model.DomainModel
+import com.heitorcolangelo.domain.common.model.PageDomainModel
+import com.heitorcolangelo.domain.common.scheduler.ExecutionThreadProvider
+import com.heitorcolangelo.domain.common.scheduler.PagedObserver
+import io.reactivex.rxjava3.core.Observable
+
+abstract class PagedUseCase<Model : DomainModel>(
+    private val threadProvider: ExecutionThreadProvider
+) : DisposableUseCase() {
+    companion object {
+        const val NO_PAGE_LOADED = -1
+    }
+
+    private var currentPage: Int = NO_PAGE_LOADED
+
+    abstract fun build(args: Args): Observable<PageDomainModel<Model>>
+
+    fun execute(args: Args, observer: PagedObserver<Model>) {
+        val observable = this.build(args)
+            .subscribeOn(threadProvider.io())
+            .observeOn(threadProvider.ui())
+            .doOnNext {
+                if (it.page <= currentPage) {
+                    observer.invalidatePages()
+                }
+                currentPage = it.page
+            }
+        disposables.add(observable.subscribeWith(observer))
+    }
+
+    /**
+     * Returns the value of the next page to be fetched based on [Args.forceRefresh] value.
+     * @return [PageDomainModel.FIRST_PAGE] if [Args.forceRefresh] is true,
+     * @return [currentPage] + 1 if [Args.forceRefresh] is false.
+     */
+    protected fun nextPage(args: Args): Int {
+        if (args.forceRefresh) {
+            currentPage = NO_PAGE_LOADED
+        }
+
+        return currentPage + 1
+    }
+
+    data class Args(val forceRefresh: Boolean = false) : UseCaseArgs
+}
