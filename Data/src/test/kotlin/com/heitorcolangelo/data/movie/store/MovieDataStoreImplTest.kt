8d753856c6c3fa5f03605135a8ed1f742da10c96
Heitor Colangelo <heitorcolangelo@gmail.com>
2021-01-17 11:44:26 -0300

    Fix Data module tests and CI test step

--- Data/src/test/kotlin/com/heitorcolangelo/data/movie/store/MovieDataStoreImplTest.kt
+++ Data/src/test/kotlin/com/heitorcolangelo/data/movie/store/MovieDataStoreImplTest.kt
@@ -13,29 +13,32 @@ class MovieDataStoreImplTest {
     private val dataStore = MovieDataStoreImpl(localDataStore, remoteDataStore)
 
     @Test
-    fun `WHEN local data is valid THEN get movies from local data store`() {
+    fun `WHEN local data is valid THEN get movie from local data store`() {
+        val movieId = "movieId"
         coEvery { localDataStore.isDataValid() } returns true
 
-        runBlocking { dataStore.getMovies(0) }
+        runBlocking { dataStore.getMovie(movieId) }
 
-        coVerify { localDataStore.getMovies(0) }
+        coVerify { localDataStore.getMovie(movieId) }
     }
 
     @Test
-    fun `WHEN local data is NOT valid THEN get movies from remote data store`() {
+    fun `WHEN local data is NOT valid THEN get movie from remote data store`() {
+        val movieId = "movieId"
         coEvery { localDataStore.isDataValid() } returns false
 
-        runBlocking { dataStore.getMovies(0) }
+        runBlocking { dataStore.getMovie(movieId) }
 
-        coVerify { remoteDataStore.getMovies(0) }
+        coVerify { remoteDataStore.getMovie(movieId) }
     }
 
     @Test
-    fun `WHEN get movies from remote data store THEN save movies`() {
+    fun `WHEN get movie from remote data store THEN save movies`() {
+        val movieId = "movieId"
         coEvery { localDataStore.isDataValid() } returns false
-        coEvery { remoteDataStore.getMovies(0) } returns mockk(relaxed = true)
+        coEvery { remoteDataStore.getMovie(movieId) } returns mockk(relaxed = true)
 
-        runBlocking { dataStore.getMovies(0) }
+        runBlocking { dataStore.getMovie(movieId) }
 
         coVerify { localDataStore.saveMovies(any()) }
     }
@@ -45,5 +48,34 @@ class MovieDataStoreImplTest {
         runBlocking { dataStore.saveMovies(listOf()) }
 
         coVerify { localDataStore.saveMovies(any()) }
+    }
+
+    @Test
+    fun `WHEN forceRefresh is true THEN get latest movie from remote data store`() {
+        val forceRefresh = true
+        coEvery { remoteDataStore.getLatestMovie(any()) } returns mockk(relaxed = true)
+
+        runBlocking { dataStore.getLatestMovie(forceRefresh) }
+
+        coVerify { remoteDataStore.getLatestMovie() }
+    }
+
+    @Test
+    fun `WHEN local data is valid THEN get latest movie from local data store`() {
+        coEvery { localDataStore.isDataValid() } returns true
+
+        runBlocking { dataStore.getLatestMovie(false) }
+
+        coVerify { localDataStore.getLatestMovie() }
+    }
+
+    @Test
+    fun `WHEN local data is NOT valid THEN get latest movie from remote data store`() {
+        coEvery { localDataStore.isDataValid() } returns false
+        coEvery { remoteDataStore.getLatestMovie(any()) } returns mockk(relaxed = true)
+
+        runBlocking { dataStore.getLatestMovie(false) }
+
+        coVerify { remoteDataStore.getLatestMovie() }
     }
 }
