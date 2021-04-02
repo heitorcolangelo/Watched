f4ef9068e128c1f9bd1766418d53cf37a15e1273
Heitor Colangelo <heitorcolangelo@gmail.com>
2021-04-02 19:26:12 +0100

    Add corner radius

--- App/src/main/kotlin/com/watched/presentation/media/MediaSectionItemBinder.kt
+++ App/src/main/kotlin/com/watched/presentation/media/MediaSectionItemBinder.kt
@@ -1,12 +1,12 @@
 package com.watched.presentation.media
 
-import androidx.recyclerview.widget.LinearLayoutManager
 import coil.load
 import coil.transform.RoundedCornersTransformation
 import com.watched.databinding.ItemMediaBinding
 import com.watched.databinding.ItemMediaSectionBinding
 import com.watched.presentation.common.list.BaseAdapter
 import com.watched.presentation.common.list.HorizontalItemDecoration
+import com.watched.presentation.common.list.HorizontalLinearLayoutManager
 import com.watched.presentation.media.model.MediaItemUiModel
 import com.watched.presentation.media.model.MediaSectionItemUiModel
 
@@ -18,11 +18,10 @@ class MediaSectionItemBinder :
             tvTitle.setText(model.title)
 
             val context = root.context
-            val layoutManager = LinearLayoutManager(context)
-            layoutManager.orientation = LinearLayoutManager.HORIZONTAL
-            rvList.layoutManager = layoutManager
-            rvList.addItemDecoration(HorizontalItemDecoration(context))
             val adapter = BaseAdapter(ItemMediaBinding::inflate, MovieItemBinder())
+
+            rvList.layoutManager = HorizontalLinearLayoutManager(context)
+            rvList.addItemDecoration(HorizontalItemDecoration(context))
             rvList.adapter = adapter
 
             adapter.submitList(model.list)
@@ -33,7 +32,7 @@ class MediaSectionItemBinder :
         override fun bindListItem(binding: ItemMediaBinding, model: MediaItemUiModel) {
             binding.mediaPoster.load(model.poster.path()) {
                 crossfade(true)
-                transformations(RoundedCornersTransformation())
+                transformations(RoundedCornersTransformation(8f))
             }
         }
     }
